d1e96a64b807548641c7d2e5aa3bf367e786f58c
Philip Karlsson Gisslow <philipkarlsson@me.com>
2019-05-22 17:41:53 +0200

    New buffer and location solved

--- rplugin/python3/yaj.py
+++ rplugin/python3/yaj.py
@@ -21,16 +21,51 @@ class Yaj(object):
         self.logstr.append('== Yaj debug ==')
 
     def log(self, s):
-        self.logstr.append(s)
+        self.logstr.append(str(s))
+
+    def open_yaj_buf(self):
+        self.nvim.command('e Yaj')
+        self.nvim.command('setlocal buftype=nofile')
+        self.nvim.command('setlocal filetype=yaj')
 
     @neovim.command("Yaj", range='', nargs='*', sync=True)
     def yaj(self, args, range):
-        self.log('Haj from yaj')
+        buf = self.nvim.current.buffer
+        window = self.nvim.current.window
+
+        # Height could be used to optimize performance?
+        window_height = window.height
+
+        # Sample positions
+        current_pos = window.cursor
+        self.nvim.command('normal! H')
+        top_pos = window.cursor
+        self.nvim.command('normal! L')
+        bottom_pos = window.cursor
+
+        # Spawn the yaj buffer
+        self.open_yaj_buf()
+
+        new_buf = self.nvim.current.buffer
+        # Paste the lines of the old buffer to the new
+        new_buf[:] = buf[:]
+
+        # Update position
+        new_window = self.nvim.current.window
+        new_window.cursor = top_pos
+        self.nvim.command('normal! zt')
+
+        diff = current_pos[0] - top_pos[0]
+        self.log(diff)
+        self.nvim.command('normal! %dj' % (diff))
+
+        # Spawn the filter bar (Use the filter from bolt)
+        # TODO: Cont here..
 
     @neovim.command("YayShowLog", range='', nargs='*', sync=True)
     def YajShowLog(self, args, range):
         self.nvim.command('e Yaj_log')
         self.nvim.command('setlocal buftype=nofile')
-        self.nvim.command('setlocal filetype=utils_log')
+        self.nvim.command('setlocal filetype=yaj_log')
         self.nvim.current.buffer.append(self.logstr)
 
